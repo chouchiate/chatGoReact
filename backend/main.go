4f1961ac992386c2c4e705969f058f4931a59142
Derick Chou <derick.chou@fluxconn.com>
2020-12-04 10:00:47 +0800

    first commit

---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var upgrader = websocket.upgrader{}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+}
+
+func main() {
+	fmt.Println("Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8000", nil)
+}
