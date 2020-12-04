6a186add480d8eb494f9f8b9a34d1c1c579e03f0
Derick Chou <derick@jubo.health>
2020-12-04 19:19:19 +0800

    Complete handle multiple clients

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,8 @@ module github.com/TutorialEdge/realtime-chat-go-react
 
 go 1.14
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.4.2
+)
+
--- backend/main.go
+++ backend/main.go
@@ -3,18 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
 )
 
-var upgrader = websocket.upgrader{}
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+V\n", err)
+	}
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+
+}
 
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
 	})
 }
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	fmt.Println("Distributed Chat App v0.05")
 	setupRoutes()
 	http.ListenAndServe(":8000", nil)
 }
